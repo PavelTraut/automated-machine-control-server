981bf117df27b578a37a5bdace8cb7ec2d4fdbc3
SazukinPavel <sazukinpavelal@gmail.com>
2023-02-04 10:49:30 +0300

    refactor app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -21,7 +19,5 @@ import { AppService } from './app.service';
       inject: [ConfigService],
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
