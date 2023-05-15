99ba7c600a2609791cb165c0bd04727d4dfc2d0c
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-05-16 00:13:32 +0300

    make save logs not required

--- src/logs/logs.service.ts
+++ src/logs/logs.service.ts
@@ -3,20 +3,25 @@ import { InjectRepository } from '@nestjs/typeorm';
 import Log from 'src/entitys/log.entity';
 import { Repository } from 'typeorm';
 import CreateLogDto from './dto/CreateLogDto';
- 
+import * as process from 'process';
+
 @Injectable()
 export default class LogsService {
   constructor(
     @InjectRepository(Log)
-    private logsRepository: Repository<Log>
+    private logsRepository: Repository<Log>,
   ) {}
- 
+
   async createLog(log: CreateLogDto) {
+    if (!process.env.SAVE_LOGS) {
+      return;
+    }
+
     const newLog = await this.logsRepository.create(log);
     await this.logsRepository.save(newLog, {
       data: {
-        isCreatingLogs: true
-      }
+        isCreatingLogs: true,
+      },
     });
     return newLog;
   }
