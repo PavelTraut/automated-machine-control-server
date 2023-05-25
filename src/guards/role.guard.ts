9c8e334c46ae245e108d58030015cc5945a72c7c
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-05-25 11:32:25 +0300

    fix role guard

--- src/guards/role.guard.ts
+++ src/guards/role.guard.ts
@@ -1,22 +1,28 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import AppRequest from '../types/AppRequest';
+import getUserLevelByRole from '../utils/getUserLevelByRole';
+import Role from '../types/Role';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    const roles: Role[] = this.reflector.get<Role[]>(
+      'roles',
+      context.getHandler(),
+    );
     if (!roles) {
       return true;
     }
+    const rolesLevels = roles.map((r) => getUserLevelByRole(r));
+
     const request: AppRequest = context.switchToHttp().getRequest();
 
     return (
-      request.user?.role === 'sudo' ||
-      (roles.includes('all') && !!request.user) ||
-      roles.includes(request.user?.role)
+      !!(rolesLevels.includes(-1) && request.user?.role) ||
+      Math.max(...rolesLevels) >= getUserLevelByRole(request.user.role)
     );
   }
 }
