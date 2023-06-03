32a54af0420bcb6f3590626b91933c12623d58fd
SazukinPavel <97695224+SazukinPavel@users.noreply.github.com>
2023-06-03 14:48:35 +0300

    Update role.guard.ts

--- src/guards/role.guard.ts
+++ src/guards/role.guard.ts
@@ -9,19 +9,19 @@ export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-//     const roles: Role[] = this.reflector.get<Role[]>(
-//       'roles',
-//       context.getHandler(),
-//     );
-//     if (!roles) {
+  const roles: Role[] = this.reflector.get<Role[]>(
+       'roles',
+       context.getHandler(),
+     );
+     if (!roles) {
       return true;
-//     }
-//     const rolesLevels = roles.map((r) => getUserLevelByRole(r));
-//     const request: AppRequest = context.switchToHttp().getRequest();
-//     return (
-//       request.user &&
-//       (!!(rolesLevels.includes(-1) && request.user?.role) ||
-//         Math.max(...rolesLevels) >= getUserLevelByRole(request.user?.role))
-//     );
+     }
+     const rolesLevels = roles.map((r) => getUserLevelByRole(r));
+     const request: AppRequest = context.switchToHttp().getRequest();
+    return (
+       request.user &&
+     (!!(rolesLevels.includes(-1) && request.user?.role) ||
+        Math.max(...rolesLevels) >= getUserLevelByRole(request.user?.role))
+    );
   }
 }
