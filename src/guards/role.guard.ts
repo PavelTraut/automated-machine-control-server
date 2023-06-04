c6bd61c91ac3501336de1547349cb6fe00e00cf6
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-06-04 11:30:01 +0300

    test guard datas

--- src/guards/role.guard.ts
+++ src/guards/role.guard.ts
@@ -18,6 +18,14 @@ export class RolesGuard implements CanActivate {
     }
     const rolesLevels = roles.map((r) => getUserLevelByRole(r));
     const request: AppRequest = context.switchToHttp().getRequest();
+
+    console.log(
+      roles,
+      rolesLevels,
+      request.user?.role,
+      Math.max(...rolesLevels),
+      getUserLevelByRole(request.user?.role),
+    );
     return (
       request.user &&
       (!!(rolesLevels.includes(-1) && request.user?.role) ||
